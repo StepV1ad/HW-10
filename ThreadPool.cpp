7f477c64f9fc2c816c8c0944d54a8f56b8058690
Stepanov Vladislav <96477530+StepV1ad@users.noreply.github.com>
2022-08-26 19:17:51 +0700

    added new file

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -1,7 +1,16 @@
 #include "ThreadPool.h"
-/*
+
 ThreadPool::ThreadPool() :
-    m_thread_count(thread::hardware_concurrency() != 0 ? thread::hardware_concurrency() : 4), m_thread_queues(m_thread_count) {}
+    m_thread_count(thread::hardware_concurrency() != 0 ? thread::hardware_concurrency() : 4), 
+    m_thread_queues(m_thread_count), m_index(0)
+{
+    start();
+}
+
+ThreadPool::~ThreadPool()
+{
+    stop();
+}
 
 void ThreadPool::start() 
 {
@@ -16,44 +25,53 @@ void ThreadPool::stop()
         task_type empty_task;
         m_thread_queues[i].push(empty_task);
     }
-    for (auto& t : m_threads) 
-        t.join();
-}*/
+    for (auto& t : m_threads)
+        if (t.joinable())
+            t.join();
+}
 
 res_type ThreadPool::push_task(FuncType f, int*& arr, int low, int high)
 {
-    lock_guard<mutex> l(m_locker);
-    TaskWithPromise twp;
-    twp.task = [=, &arr] {f(arr, low, high); };
-    res_type res = twp.prom.get_future();
-
-    m_task_queue.push(move(twp));
-    m_event_holder.notify_one();
-
+    int queue_to_push = m_index++ % m_thread_count;
+    task_type task ([=, &arr] {f(arr, low, high); });
+    res_type res = task.get_future();
+    m_thread_queues[queue_to_push].push(task);
     return res;
-    /*int queue_to_push = m_index++ % m_thread_count;
-    task_type task = [=, &arr] {f(arr, low, high); };
-    m_thread_queues[queue_to_push].push(task);*/
 }
 
-void ThreadPool::threadFunc() {
-    while (true) {
-        TaskWithPromise task_to_do;
-        {
-            unique_lock<mutex> l(m_locker);
-            if (m_task_queue.empty() && !m_work)
-                return;
-            if (m_task_queue.empty()) {
-                m_event_holder.wait(l, [&]() {return !m_task_queue.empty() || !m_work; });
-            }
-            if (!m_task_queue.empty()) {
-                task_to_do = move(m_task_queue.front());
-                m_task_queue.pop();
-            }
-        }
-        if (task_to_do.task) {
-            task_to_do.task();
-            task_to_do.prom.set_value();
-        }
+void ThreadPool::threadFunc(int qindex) 
+{
+    while (true) 
+    {
+        task_type task_to_do;
+        bool res;
+        int i = 0;
+        for (; i < m_thread_count; i++) 
+            if (res = m_thread_queues[(qindex + i) % m_thread_count].fast_pop(task_to_do))
+                break;
+        if (!res) 
+            m_thread_queues[qindex].pop(task_to_do);
+        else if (!task_to_do.valid()) 
+            m_thread_queues[(qindex + i) % m_thread_count].push(task_to_do);
+        if (!task_to_do.valid()) 
+            return;
+        task_to_do();
     }
+}
+
+void ThreadPool::pending_task()
+{
+    task_type task_to_do;
+    bool res;
+    int i = 0;
+    for (; i < m_thread_count; ++i)	
+    {
+        res = m_thread_queues[i % m_thread_count].fast_pop(task_to_do);
+        if (res) break;
+    }
+
+    if (!task_to_do.valid())	
+        std::this_thread::yield();
+    else
+        task_to_do();
 }
